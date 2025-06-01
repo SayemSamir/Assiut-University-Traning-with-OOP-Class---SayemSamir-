12469ebf0bdcde1ea820e2312d9e172eea342a60
Sayem Uddin Samir <sayemuddinsamir00@gmail.com>
2025-06-01 10:34:32 +0600

    Update E-Max.cpp

---
+++ C-sheet2_Loops/E-Max.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class Max{
+public:
+void result();
+};
+void Max::result()
+{
+	int counter; 
+	cin >> counter;
+	int max = 0;
+	for (int i = 1; i <= counter; i++)
+	{
+		int num;
+		cin >> num;
+		if (i == 1)
+		{
+			max = num;
+		}
+		if (num > max)
+		{
+			max = num;
+		}
+	}
+	cout << max << endl;
+}
+int main(){
+  max obj;
+  obj.result();
+}
